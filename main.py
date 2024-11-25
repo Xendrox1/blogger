3418a49d90a235c2ca14b4227b097903230eae5e
Xendrox <aayush.khadka62@gmail.com>
2024-11-25 14:25:23 +0545

    new things

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ def comments(id: int):
 class Blog(BaseModel):
     title: str
     body : str
-    published: Optional[bool] = None
+    published: Optional[bool] 
     
 @app.post('/blog')
 def create_blog(blog: Blog):
     return{'data':f'Blog is created with title as [{blog.title}]'} 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port = 9000)
 
